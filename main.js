a2f253b18238079cce7a2082278725761d127c41
kschuermann <schuermann.kelby@yahoo.com>
2015-11-02 22:40:49 -0600

    Displays JSON data
    jquery gets the json data and then the html then displays the data

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
+var businesses;
+function showbusiness(i){
+		if(businesses.length > 0){
+			var business = businesses[i];
+			var business_address = business.location.display_address;
+			var display_address = business_address.join();
+			$("#bizname").text(business.name);
+			$("#bizphone").text(business.display_phone);
+			$("#bizrating").text(business.rating);
+			$("#bizaddress").text(display_address);
+			
+		}
+	};
 $(document).ready(function() {
 	$.getJSON( "restaurants.txt", function(data) {
-		alert( data.total);
+		businesses = data.businesses;
+		showbusiness(0);
 	});
+	
 });	

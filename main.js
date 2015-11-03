94857d7d464c29ba5202b35fbac426c5269da3c6
UberKitten <566438+UberKitten@users.noreply.github.com>
2015-11-03 13:58:49 -0600

    Change business address joining

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function showbusiness(i){
 		if(businesses.length > 0){
 			var business = businesses[i];
 			var business_address = business.location.display_address;
-			var display_address = business_address.join();
+			var display_address = business_address.join(" ");
 			$("#bizname").text(business.name);
 			$("#bizphone").text(business.display_phone);
 			$("#bizrating").text(business.rating);
@@ -22,5 +22,4 @@ $(document).ready(function() {
 		businesses = data.businesses;
 		showbusiness(0);
 	});
-	
 });	

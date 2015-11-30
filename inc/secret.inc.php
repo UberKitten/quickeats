7186aebe4430de1fc16432aa0b3d8348bf43029f
UberKitten <566438+UberKitten@users.noreply.github.com>
2015-11-29 20:06:51 -0600

    Add MySQL constants

--- inc/secret.inc.php
+++ inc/secret.inc.php
@@ -1,8 +1,17 @@
 <?php
 
+// We're unable to access the admin for our app to set the appropriate environment variables for these
 define('CONSUMER_KEY', '');
 define('CONSUMER_SECRET','');
 define('TOKEN','');
 define('TOKEN_SECRET','');
+
+define('MYSQL_DATABASE', 'mwa3');
+
+// Pre-defined in OpenShift Environment variables
+define('MYSQL_HOST', $_ENV["OPENSHIFT_MYSQL_DB_HOST"]);
+define('MYSQL_PORT', $_ENV["OPENSHIFT_MYSQL_DB_PORT"]);
+define('MYSQL_USERNAME', $_ENV["OPENSHIFT_MYSQL_DB_USERNAME"]);
+define('MYSQL_PASSWORD', $_ENV["OPENSHIFT_MYSQL_DB_PASSWORD"]);
 
 ?>

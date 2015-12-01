31250ffb28a4971922025c30fa305034994f797f
UberKitten <566438+UberKitten@users.noreply.github.com>
2015-11-30 19:40:38 -0600

    Change rating image logic

--- main.js
+++ main.js
@@ -3,23 +3,28 @@ var x=1;
 var coords = 0;
 var zip;
 function showbusiness(i){
-		if(businesses.length > 0){
-			var business = businesses[i];
-			var business_address = business.location.display_address;
-			var display_address = business_address.join(" ");
-			$("#bizname").text(business.name);
-			$("#bizphone").text(business.display_phone);
-			$("#bizrating").text(business.rating);
-			$("#bizratingpic").attr("src",business.rating_img_url);
-			$("#bizpicture").attr("src",business.image_url);
-			if(bizratingpic == null){
-				bizratingpic = "";
-			};
-			$("#bizaddress").text(display_address);
-			$("#directionsbtn").attr("href", "https://www.google.com/maps?hl=en&q=" + display_address);
-			$('.restaurant-stars-rating').width(business.rating*76/5);
+	if(businesses.length > 0){
+		var business = businesses[i];
+		var business_address = business.location.display_address;
+		var display_address = business_address.join(" ");
+		$("#bizname").text(business.name);
+		$("#bizphone").text(business.display_phone);
+		$("#bizrating").text(business.rating);
+		$("#bizpicture").attr("src",business.image_url);
+		$("#bizratingpic").attr("src",business.rating_img_url_large);
+		if(business.rating_img_url_large == null){
+			$("#bizratingpic").hide();
 		}
-	};
+		else
+		{
+			$("#bizratingpic").show();
+		}
+		$("#bizaddress").text(display_address);
+		$("#directionsbtn").attr("href", "https://www.google.com/maps?hl=en&q=" + display_address);
+		$('.restaurant-stars-rating').width(business.rating*76/5);
+	}
+};
+
 function nextbusiness(){
     showbusiness(x);
 	x++;
